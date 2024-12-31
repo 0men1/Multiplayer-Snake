557a8275babc416934d396fd341faf00dc65ed31
HomHom1 <homahomen2@gmail.com>
2024-12-31 08:55:46 -0600

    game class

---
+++ src/components/game/GameBoard.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React from 'react';
+import { Game } from '@/logic/Game';
+
+
+interface GameBoardProps {
+    game: Game
+}
+
+const GameBoard: React.FC<GameBoardProps> = ({ game }) => {
+    return (
+        <div>
+            {} 
+        </div>
+    )
+}
+
+export default GameBoard;
